8cbe584c8d59eb263518c58ac7699f3a1c76fb64
leomoraiscam <leozonnn@gmail.com>
2020-08-24 17:57:30 -0500

    Estilizando a tabela

--- src/components/Table/styles.js
+++ src/components/Table/styles.js
@@ -5,4 +5,19 @@ export const Container = styled.div``;
 export const Table = styled.table`
   width: 500px;
   border-collapse: collapse;
+
+  th {
+    padding: 10px;
+    background: #dcdcdc;
+    text-align: left;
+  }
+
+  tbody {
+    width: 100%;
+
+    tr {
+      text-align: left;
+      border-bottom: 1px solid #dcdcdc;
+    }
+  }
 `;
